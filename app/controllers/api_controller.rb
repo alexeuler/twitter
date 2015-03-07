b618abe1596f4948580fb25a13f81372848ea274
alex@laptop <karasev.alexey@laptop>
2015-03-07 14:42:58 +0300

    fixed some bugs

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,12 +3,7 @@ require_relative '../../lib/twitter'
 class ApiController < ApplicationController
   def follow
     users = params[:data][:users]
-    begin
-      Twitter::follow users
-    rescue Exception => e
-      puts e.message
-      render json: e.message
-    end
+    Twitter::follow users
     render json: 'Ok'
   end
 
